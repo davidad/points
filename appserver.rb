13a67684482c8296447cebf157c72b074875d687
Lyla J Fischer <fischerl@dr-wily.mit.edu>
2011-01-31 02:11:04 -0500

    added a very little bit of authecation

--- appserver.rb
+++ appserver.rb
@@ -8,6 +8,19 @@ def get_or_post(path, opts={}, &block)
   post(path, opts, &block)
 end
 
+app_secret = fbf03b0a9c5175ddecc1ab01bbe5a370
+app_id = 196454990366058
+api_key = 590cdce4e47bccec1f78b5ff2729f7de
+
+get '/facebooklogin' do
+  halt 302, 'https://www.facebook.com/dialog/oauth?client_id=YOUR_APP_ID&redirect_uri=points.xvm.mit.edu/authenticate'
+end
+
+get '/authenticate' do
+  #there is a variable passed here called caode that can be used to get more stuff.
+  'not yet implemented'
+end
+
 get_or_post '/newuser' do
   content_type :json
   username = params['username']
