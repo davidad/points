dab6924b3872ef1628a045e5ece7f31965f16a1f
Lyla J Fischer <fischerl@dr-wily.mit.edu>
2011-01-31 05:39:41 -0500

    exparimenting with token

--- appserver.rb
+++ appserver.rb
@@ -21,7 +21,7 @@ get '/' do
     "<script> top.location.href = '#{dialog_url}' </script>"
   else
     token_url = "https://graph.facebook.com/oauth/access_token?client_id=#{$app_id}&redirect_uri=#{URI.encode($default_uri)}&client_secret=#{$app_secret}&code=#{params['code']}"
-    $app_token = URI.parse(URI.encode(token_url)).read
+    $app_token = URI.parse(URI.encode(token_url)).read.split('=')[1]
     $app_token
   end
 end
