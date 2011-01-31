172fb487bf9d8d9d5925c7fd6bba5761f44b231c
David Dalrymple (on tzadi) <davidad@alum.mit.edu>
2011-01-31 00:51:27 -0500

    newuser call

--- appserver.rb
+++ appserver.rb
@@ -1,8 +1,27 @@
 require 'sinatra'
 require 'json'
+require 'digest/sha1'
 set :port, 8080
 
-get '/test_json' do
+def get_or_post(path, opts={}, &block)
+  get(path, opts, &block)
+  post(path, opts, &block)
+end
+
+get_or_post '/newuser' do
   content_type :json
-  params.to_json
+  username = params['username']
+  if(!/\A\w\+\Z/=~username) then #check if username consists entirely of sane characters
+    { 'success'=>0,
+      'error'=>"Username must consist only of digits, uppercase or lowercase letters, and underscores." }.to_json
+  if(File.file?(username)) then
+    { 'success'=>0,
+      'error'=>"User #{username} already exists" }.to_json
+  else
+    apikey = Digest::SHA1.hexdigest(Time.now_to_s + rand(1000000).to_s)
+    userfile = File.new(username,"w+")
+    userfile.puts(apikey)
+    { 'success'=>1,
+      'apikey'=>apikey }.to_json
+  end
 end
