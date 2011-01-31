0434517ee12d78dc74b555b42afc466eef780f7c
Lyla J Fischer <fischerl@dr-wily.mit.edu>
2011-01-31 03:40:03 -0500

    x

--- appserver.rb
+++ appserver.rb
@@ -21,8 +21,7 @@ end
 
 get '/authenticate' do
 
-  Net::HTTP.get URI.parse("https://graph.facebook.com/oauth/access_token?client_id=#{$app_id}")
-  # "https://graph.facebook.com/oauth/access_token?client_id=#{$app_id}&redirect_uri=#{$default_url}&client_secret=#{$app_secret}&code=#{params['code']}"
+  Net::HTTPS.get URI.parse(URI.encode("https://graph.facebook.com/oauth/access_token?client_id=#{$app_id}&redirect_uri=#{$default_url}&client_secret=#{$app_secret}&code=#{params['code']}"))
 end
 
 def failure(msg)
